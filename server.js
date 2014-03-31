ede272a2b192bf2de77b44b267a9fa3dc533991d
Lee Gillentine <lgillentine@gmail.com>
2014-03-30 21:34:44 -0500

    Finally got basics set up

--- server.js
+++ server.js
@@ -1,6 +1,51 @@
-/*jslint node: true, sloppy: true, white: true */
+/*jslint node: true, sloppy: true, white: true, vars: true */
 
 var net = require('net');
-var server = net.createServer(function(socket) {
-  socket.write('Welcome to the Telnet server!');
-}).listen(1337);
+var sockets = [];
+
+
+function cleanInput(data) {
+  return data.toString().replace(/(\r\n|\n|\r)/gm,"");
+}
+
+function recieveData(socket, data) {
+  var cleanData = cleanInput(data);
+  console.log(data);
+  console.log(cleanData);
+  if (cleanData == "@quit") {
+    socket.end('Goodbye!\n');
+  }
+  else {
+    for (var i = 0; i<sockets.length; i++){
+      if (sockets[i] !== socket) {
+        sockets[i].write(data);
+      }
+      if (sockets[i] == socket) {
+        sockets[i].write("You said: " + data);
+      }
+    }
+  }
+}
+
+function closeSocket(socket) {
+  var i = sockets.indexOf(socket);
+  if (i != -1) {
+    sockets.splice(i, 1);
+  }
+}
+
+function newSocket(socket) {
+  sockets.push(socket);
+  socket.write('Welcome!\n');
+  socket.on('data', function(data) {
+    recieveData(socket, data);
+  });
+  socket.on('end', function() {
+    closeSocket(socket);
+  });
+}
+
+var server = net.createServer(newSocket);
+
+
+server.listen(1337);
