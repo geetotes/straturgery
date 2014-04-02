1bd38c599633b62684dedec7ea14881280a9e1c3
Lee Gillentine <lgillentine@gmail.com>
2014-04-01 20:59:14 -0500

    Haha, clever tagline for the game

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 /*jslint node: true, sloppy: true, white: true, vars: true */
 
 var net = require('net');
+var colors = require('colors');
 var sockets = [];
+
+Date.prototype.getMonthName = function() {
+  var months = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  return months[this.getMonth()];
+};
 
 //turn is an int
 //time should go from a start date
@@ -20,10 +26,30 @@ function cleanInput(data) {
   return data.toString().replace(/(\r\n|\n|\r)/gm,"");
 }
 
+function drawWelcome(){
+  var welcome = "\x1B[31m";
+  welcome += "-----------------------------------------\n";
+  welcome += "             Welcome to\n";
+  welcome += "             STRATURGERY\n";
+  welcome += "-----------------------------------------\n";
+  welcome += "\x1B[39m";
+  welcome += "\x1B[3m\x1B[37m         The Game of Pulling Out\x1B[39m\x1B[23m";
+
+  return welcome;
+}
+
 function recieveData(socket, data, turn) {
+  //increment time
+  //output time
+  var currentDate = incrementTime(turn);
+  socket.write("Today's Date: [" + currentDate.getMonthName() + " " + currentDate.getFullYear() +"]\n".zebra);
+
+
+
+  //need to find a better way to catch the quit command
   var cleanData = cleanInput(data);
   if (cleanData == "@quit") {
-    socket.end('Goodbye!\n');
+    socket.end('\x1B[44mGoodbye!\x1B[49m\n');
   }
   else {
     for (var i = 0; i<sockets.length; i++){
@@ -31,14 +57,10 @@ function recieveData(socket, data, turn) {
         sockets[i].write(data);
       }
       if (sockets[i] == socket) {
-        sockets[i].write("You said: " + data);
+        //sockets[i].write("You said: " + data);
       }
     }
   }
-  //increment time
-  //output time
-  currentDate = incrementTime(turn);
-  socket.write("\tDate: " + currentDate.toDateString());
 }
 
 function closeSocket(socket) {
@@ -51,9 +73,10 @@ function closeSocket(socket) {
 function newSocket(socket) {
   var turn = 0;
   sockets.push(socket);
-  socket.write('Welcome!\n');
+  socket.write(drawWelcome() + '\n');
   socket.on('data', function(data) {
     recieveData(socket, data, turn);
+    //should prob not increment every time input goes in
     turn += 1;
   });
   socket.on('end', function() {
