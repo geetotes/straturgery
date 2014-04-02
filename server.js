161eb23e1fbb9aae88dc5d5de2392ff2d655365c
Lee Gillentine <lgillentine@gmail.com>
2014-04-01 20:06:28 -0500

    Yay, basic game loop working!

--- server.js
+++ server.js
@@ -3,15 +3,25 @@
 var net = require('net');
 var sockets = [];
 
+//turn is an int
+//time should go from a start date
+function incrementTime(turn) {
+  //months are zero based
+  startDate = new Date(2007, 0, 1);
+  //eventually, we will use the endDate condition to determine the end of the game
+  endDate = new Date(2011, 0, 1);
+  currentDate = new Date(startDate.getFullYear(), turn, startDate.getDate());
+  return currentDate;
+
+
+}
 
 function cleanInput(data) {
   return data.toString().replace(/(\r\n|\n|\r)/gm,"");
 }
 
-function recieveData(socket, data) {
+function recieveData(socket, data, turn) {
   var cleanData = cleanInput(data);
-  console.log(data);
-  console.log(cleanData);
   if (cleanData == "@quit") {
     socket.end('Goodbye!\n');
   }
@@ -25,6 +35,10 @@ function recieveData(socket, data) {
       }
     }
   }
+  //increment time
+  //output time
+  currentDate = incrementTime(turn);
+  socket.write("\tDate: " + currentDate.toDateString());
 }
 
 function closeSocket(socket) {
@@ -35,10 +49,12 @@ function closeSocket(socket) {
 }
 
 function newSocket(socket) {
+  var turn = 0;
   sockets.push(socket);
   socket.write('Welcome!\n');
   socket.on('data', function(data) {
-    recieveData(socket, data);
+    recieveData(socket, data, turn);
+    turn += 1;
   });
   socket.on('end', function() {
     closeSocket(socket);
