b38a2ab76ee9a7ab60e90e8de6b510ddc48e311e
Lee Gillentine <lgillentine@gmail.com>
2014-04-03 18:23:11 -0500

    Starting on some GUI code

--- server.js
+++ server.js
@@ -18,8 +18,14 @@ function incrementTime(turn) {
   endDate = new Date(2011, 0, 1);
   currentDate = new Date(startDate.getFullYear(), turn, startDate.getDate());
   return currentDate;
+}
 
-
+function drawBreak(cols, symbol) {
+  var breaker = "";
+  for(var i = 1; i < cols; i++) {
+    breaker += symbol;
+  }
+  return breaker += "\n";
 }
 
 function cleanInput(data) {
@@ -28,14 +34,24 @@ function cleanInput(data) {
 
 function drawWelcome(){
   var welcome = "\x1B[31m";
-  welcome += "-----------------------------------------\n";
+  welcome += drawBreak(80, "-");
   welcome += "             Welcome to\n";
   welcome += "             STRATURGERY\n";
-  welcome += "-----------------------------------------\n";
+  welcome += drawBreak(80, "-");
   welcome += "\x1B[39m";
   welcome += "\x1B[3m\x1B[37m         The Game of Pulling Out\x1B[39m\x1B[23m\n";
 
   return welcome;
+}
+
+function drawNewsRoom(){
+  var newsRoom = "";
+  newsRoom += "TODAYS HEADLINES\n";
+  newsRoom += drawBreak(80, "-");
+  newsRoom += "WASHINGTON DC: Aliens elected president\n";
+  newsRoom += "NEW YORK CITY: Bloomberg re-elected\n";
+
+  return newsRoom;
 }
 
 //this will eventually take some type of decision menu object that will help out with highlighting
@@ -86,7 +102,9 @@ function newSocket(socket) {
   var turn = 0;
   sockets.push(socket);
   socket.write(drawWelcome());
+  //should figure out to start a new game or continue an old one somewhere here
   socket.on('data', function(data) {
+    socket.write(drawNewsRoom());
     socket.write(drawDecisionMenu());
     recieveData(socket, data, turn);
     //should prob not increment every time input goes in
